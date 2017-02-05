00e449282c327465863b5634d4279d8137423c11
Aaron <you@example.com>
2017-02-05 15:02:46 +0800

    add the readline function

--- mdtohtml.c
+++ mdtohtml.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<memory.h>
+
 #define rplc(x,y){strcpy(s[x].tag,y);s[x].size=strlen(y);}
 #define nmemb 1000	//写入nmemb数目
+#define LEN 101
+
+void check(){
+    ;
+} //检查读取字符串是否含有markdown语法
 
 void nl(FILE *fp){
     fprintf(fp,"\r\n");
     }	//写入换行
+
+void inite(char *a){
+	memset(a,0,sizeof(a));
+}	//初始化数组
 
 struct tt{
 	char tag[100];
@@ -34,24 +45,27 @@ void replace(){
 	rplc(17,"</li>")
 	rplc(18,"<ul>"); //* space && + space && - space
 	rplc(19,"</ul>");	//无序列表
-rplc(20,"<em>"); //斜体 *text* && _text_
+	rplc(20,"<em>"); //斜体 *text* && _text_
 	rplc(21,"</em>");
 	rplc(22,"<strong>"); //粗体 **text** && __text__
 	rplc(23,"</strong");
 	rplc(24,"<img src=\" "); //img
 	rplc(25,"\" />");
-}
+}			//	漏写表格，记得补
+
 int main(){
     replace();
-	char md1[100];
-	char html2[100];
+	char md1[LEN];
+	char html2[LEN];
+	char buffer[LEN];
+	inite(buffer);
 	FILE *fp1;
 	FILE *fp2;
 	//输入markdown名，输出html名
+	printf("plz enter that file route you want to transfer for:%s\n",md1);
 	scanf("%s",md1);
-	//printf("%s\n",md1);
+	printf("plz enter the file name that %s\n",html2);
 	scanf("%s",html2);
-	//printf("%s\n",html2);
 	if((fp1=fopen(md1,"r"))==NULL){	//打开 markdown 文件
 		printf("%s doesn't exist\n",md1);
 		exit(0);
@@ -61,11 +75,18 @@ int main(){
 		printf("%s doesn't exist\n",html2);
 		exit(0);
 	}
+
+	do{
+       fgets(buffer,100,fp1);
+       check();
+       //printf("%s\n",buffer);
+       inite(buffer);
+       }while(fgetc(fp1)!= EOF);
+
 	/*写入函数*/
 	//fwrite(s[24].tag,sizeof(s[24]),1,fp2);
-	nl(fp1);
+	//nl(fp1);
 	//fwrite(s[22].tag,sizeof(s[23]),1,fp2);
 	fclose(fp1);
 	fclose(fp2);
 }
-
